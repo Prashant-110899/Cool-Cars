ebb1d180dc8c02ab884b3b626834605333c69ac4
Prashant-110899 <64947417+Prashant-110899@users.noreply.github.com>
2020-08-06 12:33:15 +0530

    Add files via upload

---
+++ hig/calc.js
@@ -0,0 +1,17 @@
+// var num1=document.getElementById('x');
+// var num2=document.getElementById('y');
+// var resultf=document.getElementById('result');
+// var form=document.getElementById('xpercenty');
+//
+// form.addEventListener('submit', function(event){
+//   if(!num1.value||!num2.value) alert("Please enter the values");
+//   else{
+//   var x = parseFloat(num1.value);
+//   var y = parseFloat(num2.value);
+//   var result = x/y;
+//   var percent = result * 100;
+//
+//   resultf.innerText = "Your answer is: " + percent + "%";
+//   event.preventDefault();
+// }
+// });
---
+++ hig/learn.js
@@ -0,0 +1,6 @@
+var name="Jack";
+var age=23;
+
+var message="HI its me" + name + ", " + age;
+
+console.log(message);
